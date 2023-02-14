0fd114b47049f12423afffb3d06f2561d005567c
toumangd <toumangd@tcd.ie>
2023-02-14 15:03:01 +0000

    added matcher function v0.1

---
+++ server/utils/matcher.js
@@ -0,0 +1,22 @@
+const Fuse = require("fuse.js");
+
+export default function matcher(inputs) {
+    const options = {
+      includeScore: true,
+      threshold: 0.1,
+      keys: ["name.*"],
+    };
+  
+    const fuse = new Fuse(data, options);
+  
+    var results = [];
+  
+    inputs.forEach((input) => {
+      var search = fuse.search(input);
+      if (search.length > 0) {
+        results.push(search[0].item);
+      }
+    });
+  
+    return results;
+  }
