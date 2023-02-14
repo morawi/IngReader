a5942b8934d556cc3f4f4626629b9842210b033a
Aritro De <98959767+AritroDe@users.noreply.github.com>
2023-02-14 15:02:21 +0000

    Update parser.js

--- server/utils/parser.js
+++ server/utils/parser.js
@@ -1,1 +1,24 @@
+const { Configuration, OpenAIApi } = require("openai");
 
+async function parseText(text) {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const prompt = fs.readFileSync("./prompt.txt", "utf8");
+
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: prompt + text + `\n\nIngredients 3: `,
+    temperature: 0.7,
+    max_tokens: 512,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  return response.data.choices[0].text;
+}
+
+exports.parseText = parseText;
