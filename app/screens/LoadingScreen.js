da50eef7dd0dd6bc7e4dab5ec7cd4b4137e454d4
Louiskq <102479386+Louiskq@users.noreply.github.com>
2023-03-30 21:35:37 +0100

    update loading screen
    change from link to asset

--- app/screens/LoadingScreen.js
+++ app/screens/LoadingScreen.js
@@ -1,11 +1,15 @@
-import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { FontSize, FontFamily } from "../GlobalStyles";
+import React, { useState, useEffect } from 'react';
+import { Text, View, TouchableOpacity, Linking, Platform, StyleSheet, Image } from 'react-native';
+
+
 
 const LoadingScreen = () => {
   return (
+  
+  
+  
     <View style={styles.loadingscreen}>
-      <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjAxYjZlZDI4ZGU4YzFlYTQ3NDE5YWZlOWYxZTA1NTYyNDViMjI4ZiZjdD1n/294BQwZ83ocMRGBdP3/giphy.gif" alt="React Image" style={{ alignSelf: 'center' }} />
+       <Image source = {require('/assets/loadingScreen.gif')} /> 
     </View>
   );
 };
@@ -15,13 +19,11 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 370,
     left: 65,
-    fontSize: FontSize.size_base,
-    fontFamily: FontFamily.aBeeZeeRegular,
-    color: "#000",
     textAlign: "left",
+    color: "#c6ffc5",
   },
   loadingscreen: {
-    backgroundColor: "#c6ffc5",
+     backgroundColor: "#c6ffc5",
     flex: 1,
     width: "100%",
     height: 800,
