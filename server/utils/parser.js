2565e45ad3df7ce6db94da5b1fd96ecc29fcb8d4
ridish05 <kollir@tcd.ie>
2023-04-01 01:16:14 +0500

    feat: added parseIngredients function

--- server/utils/parser.js
+++ server/utils/parser.js
@@ -1,6 +1,42 @@
 const { Configuration, OpenAIApi } = require("openai");
 var fs = require("fs");
 
+function parseIngredients(text) {
+  // convert the input text to lowercase
+  text = text.toLowerCase();
+
+  // find the start index of the ingredients list
+  const startIndex = text.indexOf("ingredients:");
+  if (startIndex === -1) {
+    return []; // return empty array if "ingredients:" not found
+  }
+
+  // calculate the end index of the ingredients list based on the first dot
+  const endIndex = text.indexOf(".", startIndex);
+  if (endIndex === -1) {
+    return []; // return empty array if no dot found after start index
+  }
+
+  // extract the ingredients substring and remove unnecessary characters
+  ingredientsString = text.substring(startIndex + "ingredients:".length, endIndex);
+  cleanedIngredientsString = ingredientsString.replace(/\n/g, "").replace(/\r/g, "").trim();
+  // remove any hyphens from ingredient names
+  cleanedIngredientsString = cleanedIngredientsString.replace(/([a-z])-([a-z])/g, "$1$2");
+
+  // split the cleaned ingredients string into an array of ingredients
+  let ingredients = cleanedIngredientsString.split(/,|(|)|[|]/);
+  ingredients = ingredients.map(ingredient => ingredient.trim()).filter(ingredient => ingredient !== "" && isNaN(ingredient));
+
+  // capitalize the first letter of each word in the ingredients array
+  const capitalizedIngredients = ingredients.map(ingredient => {
+      const words = ingredient.trim().split(" ");
+      const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+      return capitalizedWords.join(" ");
+    });
+
+  return capitalizedIngredients;
+}
+
 export default async function parser(text) {
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -11,7 +47,7 @@ export default async function parser(text) {
 
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: prompt + text + `\n\nIngredients 3: `,
+    prompt: prompt + text + `\n\nIngredients: `,
     temperature: 0.7,
     max_tokens: 512,
     top_p: 1,
@@ -19,5 +55,7 @@ export default async function parser(text) {
     presence_penalty: 0,
   });
 
-  return response.data.choices[0].text;
+  const ingredients = parseIngredients(response.data.choices[0].text);
+
+  return ingredients;
 }
