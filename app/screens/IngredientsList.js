63ae64f651f193c277bd1e2a9fd25d03fcc111b7
capricorn05 <robert@hebeadventures.com>
2023-04-01 15:28:07 +0100

    search added

--- app/screens/IngredientsList.js
+++ app/screens/IngredientsList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { KeyboardAvoidingView, Platform, StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, View, TouchableOpacity, ScrollView, TextInput } from 'react-native';
 import SingleIngredient from "./SingleIngredient";
 
 const ingredientsListData = require('../static/ingredients.json');
@@ -9,13 +9,18 @@ const PAGE_SIZE = 20;
 const IngredientsList = () => {
   
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchText, setSearchText] = useState("");
 
-  const totalPages = Math.ceil(Object.values(ingredientsListData).filter(item => item.name.en).length / PAGE_SIZE);
+  const allIngredients = Object.values(ingredientsListData).filter(item => item.name.en);
+
+  const filteredIngredients = allIngredients.filter(item => item.name.en.toLowerCase().includes(searchText.toLowerCase()));
+
+  const totalPages = Math.ceil(filteredIngredients.length / PAGE_SIZE);
 
   const start = (currentPage - 1) * PAGE_SIZE;
   const end = start + PAGE_SIZE;
 
-  const ingredientsArray = Object.values(ingredientsListData).filter(item => item.name.en).slice(start, end);
+  const ingredientsArray = filteredIngredients.slice(start, end);
 
   const handlePreviousPage = () => {
     if (currentPage > 1) {
@@ -29,8 +34,21 @@ const IngredientsList = () => {
     }
   };
 
+  const handleSearchTextChange = (text) => {
+    setSearchText(text);
+    setCurrentPage(1);
+  };
+
   return (
     <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search ingredients"
+          value={searchText}
+          onChangeText={handleSearchTextChange}
+        />
+      </View>
       <ScrollView>
         <View style={styles.ingredientWrapper}>
             <Text style={styles.sectionTitle}>All Ingredients</Text>
@@ -41,12 +59,6 @@ const IngredientsList = () => {
               </View>
             ))}
           </View>
-
-        </View>
-      </ScrollView>
-
-      <View style={styles.footerContainer}>
-        <KeyboardAvoidingView behavior={Platform.os === 'ios' ? "padding" : "height"}  style={styles.footerContent}>
           <View style={{justifyContent: 'space-between', flexDirection: 'row', width: '50%'}}>
             <TouchableOpacity onPress={handlePreviousPage}>
               <Text style={currentPage > 1 ? styles.pageButton : styles.disabledPageButton}>{'<'}</Text>
@@ -56,9 +68,9 @@ const IngredientsList = () => {
               <Text style={currentPage < totalPages ? styles.pageButton : styles.disabledPageButton}>{'>'}</Text>
             </TouchableOpacity>
           </View>
-        </KeyboardAvoidingView>
-      </View>
+        </View>
 
+      </ScrollView>
 
     </View>
 
@@ -110,6 +122,33 @@ const styles = StyleSheet.create({
   disabledPageButton: {
     fontSize: 18,
     color: 'gray',
+  },
+  searchContainer: {
+    marginTop: 20,
+    marginBottom: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  searchInput: {
+    flex: 1,
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginRight: 10,
+  },
+  searchButton: {
+    backgroundColor: 'blue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  searchButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
   },
 });
 
