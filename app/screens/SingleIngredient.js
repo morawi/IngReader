ac1de29c73dc0a94814faeb3b0d2d58cb1fc9beb
capricorn05 <robert@hebeadventures.com>
2023-04-01 15:01:59 +0100

    update to read list from json file

--- app/screens/SingleIngredient.js
+++ app/screens/SingleIngredient.js
@@ -2,34 +2,35 @@ import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const SingleIngredient = (props) => {
-  return (
-    <View style={styles.item}>
-        <View style={styles.itemContent}>
-            <Text style={styles.itemNameText}>{ props.item.name.en } </Text>
-            <Text style={styles.itemBodyText}>Food code: { props.item.ciqual_food_code.en }</Text>
-            <Text style={styles.itemBodyText}>Carbon footprint: { props.item.carbon_footprint_fr_foodges_value.fr }</Text>
-            <Text style={styles.itemBodyText}>Food code: { props.item.ciqual_food_code.en }</Text>
-            <Text style={styles.itemBodyText}>Carbon footprint: { props.item.carbon_footprint_fr_foodges_value.fr }</Text>
-            <Text style={styles.itemBodyText}>Food code: { props.item.ciqual_food_code.en }</Text>
-            <Text style={styles.itemBodyText}>Carbon footprint: { props.item.carbon_footprint_fr_foodges_value.fr }</Text>
-            <Text style={styles.itemBodyText}>Food code: { props.item.ciqual_food_code.en }</Text>
-            <Text style={styles.itemBodyText}>Carbon footprint: { props.item.carbon_footprint_fr_foodges_value.fr }</Text>
-            <Text style={styles.itemBodyText}>Food code: { props.item.ciqual_food_code.en }</Text>
-            <Text style={styles.itemBodyText}>Carbon footprint: { props.item.carbon_footprint_fr_foodges_value.fr }</Text>
-            <Text style={styles.itemBodyText}>Food code: { props.item.ciqual_food_code.en }</Text>
-            <Text style={styles.itemBodyText}>Carbon footprint: { props.item.carbon_footprint_fr_foodges_value.fr }</Text>
-            <Text style={styles.itemBodyText}>Food code: { props.item.ciqual_food_code.en }</Text>
-            <Text style={styles.itemBodyText}>Carbon footprint: { props.item.carbon_footprint_fr_foodges_value.fr }</Text>
-            <Text style={styles.itemBodyText}>Food code: { props.item.ciqual_food_code.en }</Text>
-            <Text style={styles.itemBodyText}>Carbon footprint: { props.item.carbon_footprint_fr_foodges_value.fr }</Text>
-            <Text style={styles.itemBodyText}>Food code: { props.item.ciqual_food_code.en }</Text>
-            <Text style={styles.itemBodyText}>Carbon footprint: { props.item.carbon_footprint_fr_foodges_value.fr }</Text>
-            <Text style={styles.itemBodyText}>Food code: { props.item.ciqual_food_code.en }</Text>
-            <Text style={styles.itemBodyText}>Carbon footprint: { props.item.carbon_footprint_fr_foodges_value.fr }</Text>
-        </View>
-    </View>
+    return (
+        <View style={styles.item}>
+            <View style={styles.itemContent}>
+                <Text style={styles.itemNameText}>{props.item.name.en}</Text>
+                {props.item.additives_classes && props.item.additives_classes.en &&
+                    <Text style={styles.itemBodyText}>Additive Classes: {props.item.additives_classes.en.split(',').map(additive => additive.split(':')[1]).join(', ')}</Text>
+                }
+                {props.item.vegan && props.item.vegan.en &&
+                    <Text style={styles.itemBodyText}>Vegan: {props.item.vegan.en}</Text>
+                }
+                {props.item.vegetarian && props.item.vegetarian.en &&
+                    <Text style={styles.itemBodyText}>Vegetarian: {props.item.vegetarian.en}</Text>
+                }
+                {props.item.wikidata && props.item.wikidata.en &&
+                    <Text style={styles.itemBodyText}>Wikidata: {props.item.wikidata.en}</Text>
+                }
+                {/* {props.item.parents && props.item.parents.en &&
+                    <Text style={styles.itemBodyText}>Parents: {props.item.parents.en}</Text>
+                } */}
+                {props.item.parents && props.item.parents.length > 0 &&
+                    <Text style={styles.itemBodyText}>Parents: {props.item.parents.map(p => p.replace('en:', '')).join(", ")}</Text>
+                }
 
-  );
+                {props.item.ciqual_food_name && props.item.ciqual_food_name.en &&
+                    <Text style={styles.itemBodyText}>Ciqual Food Name: {props.item.ciqual_food_name.en}</Text>
+                }
+            </View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
